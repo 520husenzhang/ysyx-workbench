0b180ea30a69892b8d0f651b676179a2ac5e4220
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-02 10:32:20 +0800

    >  sim RTL ysyx_22040422 张虎森 Linux tiger-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:32:20 up 52 min,  1 user,  load average: 0.80, 0.66, 0.60

--- npc/obj_dir/Vtop__Syms.cpp
+++ npc/obj_dir/Vtop__Syms.cpp
@@ -18,8 +18,8 @@ Vtop__Syms::Vtop__Syms(VerilatedContext* contextp, const char* namep,Vtop* model
     , TOP(namep)
 {
     // Configure time unit / time precision
-    _vm_contextp__->timeunit(-12);
-    _vm_contextp__->timeprecision(-12);
+    _vm_contextp__->timeunit(-6);
+    _vm_contextp__->timeprecision(-9);
     // Setup each module's pointers to their submodules
     // Setup each module's pointer back to symbol table (for public functions)
     TOP.__Vconfigure(this, true);
