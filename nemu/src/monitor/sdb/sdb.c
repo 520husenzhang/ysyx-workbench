e89a5b08d1ccc580c0981a880f8f3ff5479297f7
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-07 19:09:44 +0800

    >  compile NEMU ysyx_22040422 张虎森 Linux tiger-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:09:44 up  1:05,  1 user,  load average: 0.99, 0.86, 1.02

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -80,7 +80,7 @@ static int cmd_x(char *args){
 sscanf(args,"%d %x",&len,&addr);  
 // printf("%d, %d",  len,addr );
 
- printf( "%lx",vaddr_read(addr,len));
+ printf( "%#lx",vaddr_read(addr,len));
 
 
 
