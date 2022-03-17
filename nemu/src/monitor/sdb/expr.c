2c225bbc74a9bcb43cf6278bebcdf6a802210085
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-17 20:05:30 +0800

    >  compile NEMU ysyx_22040422 张虎森 Linux tiger-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:05:30 up  2:04,  1 user,  load average: 0.26, 0.81, 1.56

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -222,7 +222,7 @@ uint32_t  regex_eval(int p, int q){
    int OP;   
   if (p > q) {
     /* Bad expression */
-    printf("error happen, p>q!!!");  
+    printf("error happen, p>q!  P==%d,Q==%d",p,q);  
      //assert(0);
     }
   else if (p == q) {
