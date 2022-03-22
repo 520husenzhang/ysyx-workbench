c3369d3be09e84c96c35b7ed4429db635c432751
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 22:19:06 +0800

    >  compile NEMU ysyx_22040422 张虎森 Linux tiger-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:19:06 up  1:48,  1 user,  load average: 1.61, 1.67, 1.56

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -39,13 +39,13 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
 static int decode_exec(Decode *s) {
   word_t dest = 0, src1 = 0, src2 = 0;
   s->dnpc = s->snpc;
-
+//操作数decode
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* body */ ) { \
   decode_operand(s, &dest, &src1, &src2, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
 }
-
+   //下面是执行
   INSTPAT_START();
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = src1 + s->pc);
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + src2, 8));
