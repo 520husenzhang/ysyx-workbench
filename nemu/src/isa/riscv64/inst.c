93e3c85fa808d02dbb7886df9a3d0e4f526e0593
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-24 10:30:53 +0800

    >  compile NEMU ysyx_22040422 张虎森 Linux tiger-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:30:53 up  3:10,  1 user,  load average: 1.78, 1.47, 1.37

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -23,7 +23,7 @@ enum {
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
-static word_t immJ(uint32_t i) { return  (SEXT(BITS(i,31,31),1)<<20)|(BITS(i,30,21)<<1)|(BITS(i,20,20)<<11)|(BITS(i,19,12)<<12); }  //mm[20|10:1|11|19:12]  rd  opcode
+static word_t immJ(uint32_t i) { return  ( (SEXT(BITS(i,31,31),1)<<20)|(BITS(i,30,21)<<1)|(BITS(i,20,20)<<11)|(BITS(i,19,12)<<12) )&(-1); }  //mm[20|10:1|11|19:12]  rd  opcode
 static word_t immB(uint32_t i) { return  ((SEXT(BITS(i,31,31),1)<<12)|(BITS(i,30,25)<<5)|(BITS(i,11,7)<<1)|(BITS(i,7,7)<<11)); } //imm[12|10:5]  rs2  rs1  funct3  imm[4:1|11]  opcode
 
 
