3ddba521f3c59b31e1f8b015df26876efdc70e99
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-01 10:52:33 +0800

    >  compile NEMU ysyx_22040422 张虎森 Linux tiger-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:52:33 up  3:28,  1 user,  load average: 0.74, 0.73, 0.70

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -1,15 +1,14 @@
-#include "Vtop.h"
+#include "Vrisc_v.h"
 #include <verilated.h>
 #include "verilated_vcd_c.h"
 
 VerilatedContext* contextp = NULL;  //仿真环境 
 VerilatedVcdC* tfp = NULL;  //  波形
 
-static Vtop* top;   //DUT  !!
+static Vrisc_v* top;   //risc_v DUT  !!
 
 //布进和写波形
 void step_and_dump_wave(){
-  top->eval();
   contextp->timeInc(1);
   tfp->dump(contextp->time());
 }
@@ -17,8 +16,8 @@ void step_and_dump_wave(){
 void sim_init(){
   contextp = new VerilatedContext;
   tfp = new VerilatedVcdC;
-  top = new Vtop;
-  contextp->traceEverOn(true);
+  top = new Vrisc_v;
+  contextp->traceEverOn(true);  //打开 波形  
   top->trace(tfp, 99);
   tfp->open("dump.vcd");
 }
@@ -30,12 +29,32 @@ void sim_exit(){
 
 //主函数 
 int main() {
+   //仿真环境  初始化  
   sim_init();
-  //对DUT的连接和操作
-   top->a= 0; top->b=0 ;    step_and_dump_wave();
-   top->a= 0; top->b= 1;    step_and_dump_wave();
-   top->a= 1; top->b=0 ;    step_and_dump_wave();
-   top->a= 1; top->b= 1;    step_and_dump_wave();
+  //对DUT的连接
+    top->clk=0;
+    top->rst_n=1;
+
+    //loop 
+  while (!contextp->gotFinish()) {
+
+     step_and_dump_wave();
+       //时钟翻转
+       top->clk = !top->clk; 
+        //复位
+        if (!top->clk) {
+            if (contextp->time() > 3 && contextp->time() < 10) {
+                top->rst_n = 0;  // Assert reset
+            } else {
+                top->rst_n = 1;  // Deassert reset
+            }
+        }
+ 
+      // Evaluate model
+       top->eval();
+ 
+  }
+    //退出仿真
     sim_exit();
 
 
