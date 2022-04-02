08e07aeeec43e2acb19c5a799fa9625a4dc14d24
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-02 10:06:19 +0800

    >  compile NEMU ysyx_22040422 张虎森 Linux tiger-virtual-machine 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:06:19 up 59 min,  3 users,  load average: 0.62, 0.38, 0.38

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -31,7 +31,7 @@ static word_t immS(uint32_t i) { return   SEXT  ( (BITS(i, 31, 25) << 5) | BITS(
 static word_t immJ(uint32_t i) { return  SEXT( (BITS(i,31,31)<<20)|(BITS(i,30,21)<<1)|(BITS(i,20,20)<<11)|(BITS(i,19,12)<<12) ,21); }  //mm[20|10:1|11|19:12]  rd  opcode
 
    //sb  立即数
-static int64_t immB (uint32_t i) { return  SEXT  ( ( BITS(i,31,31)<<12)|(BITS(i,30,25)<<5)|(BITS(i,11,8)<<1)|(BITS(i,7,7)<<11),13); } //imm[12|10:5]  rs2  rs1  funct3  imm[4:1|11]  opcode
+static word_t immB (uint32_t i) { return  SEXT  ( ( BITS(i,31,31)<<12)|(BITS(i,30,25)<<5)|(BITS(i,11,8)<<1)|(BITS(i,7,7)<<11),13); } //imm[12|10:5]  rs2  rs1  funct3  imm[4:1|11]  opcode
 
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
