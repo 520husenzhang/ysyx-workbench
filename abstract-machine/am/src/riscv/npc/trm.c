b8353eb967b8e2bb938137e78bfa930c4da37b0d
张虎森 <1376495760@qq.com>
2022-04-10 14:38:37 +0800

    pa2

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -7,7 +7,7 @@ int main(const char *args);
 extern char _pmem_start;
 #define PMEM_SIZE (128 * 1024 * 1024)
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
-
+//结构用于指示堆区的起始和末尾
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
 #define MAINARGS ""
@@ -16,11 +16,11 @@ static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
 }
-
+ //结束程序的运行
 void halt(int code) {
   while (1);
 }
-
+  ///TRM相关的初始化工作
 void _trm_init() {
   int ret = main(mainargs);
   halt(ret);
