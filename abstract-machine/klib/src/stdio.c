e163ce24d9295e72e1ea15370349d41da51b833c
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-12 21:32:13 +0800

    >  compile NEMU ysyx_22040422 张虎森 Linux tiger-virtual-machine-2 5.11.0-16-generic #17-Ubuntu SMP Wed Apr 14 20:12:43 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:32:13 up  3:15,  1 user,  load average: 0.47, 0.78, 0.86

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,7 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -13,10 +14,15 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+int mysprintf(char * str, const char * format, ...)
+{
+    va_list ap;
+    int ret = -1;
+    va_start(ap, format);
+    ret = vsprintf(str, format, ap);
+    va_end(ap);
+    return ret;
 }
-
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
 }
